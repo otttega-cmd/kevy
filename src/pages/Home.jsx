19472152c6a7264efcbc4f2a7edb657939ad4b5e
Otega <ot.ayomide@gmail.com>
2021-11-19 17:03:24 +0100

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Announcement from '../components/Announcement'
+import Slider from '../components/Slider'
+
+
+export const Home = () => {
+    return (
+        <div>
+            <Announcement />
+            <Navbar />
+            <Slider />
+        </div>
+        
+    )
+}
